98c38dcfa3594e469b12d204ad0961181935357b
swrush98 <swrush98@yahoo.com>
2023-07-31 20:18:23 -0500

    Add states definitions adapted from Hearts example (removing card exchanging)

--- states.inc.php
+++ states.inc.php
@@ -94,6 +94,47 @@ $machinestates = array(
     ), 
 
 */    
+    20 => array(
+        "name" => "newHand",
+        "description" => "",
+        "type" => "game",
+        "action" => "stNewHand",
+        "updateGameProgression" => true,   
+        "transitions" => array( "" => 30 )
+    ),    
+
+        
+    30 => array(
+        "name" => "newTrick",
+        "description" => "",
+        "type" => "game",
+        "action" => "stNewTrick",
+        "transitions" => array( "" => 31 )
+    ),       
+    31 => array(
+        "name" => "playerTurn",
+        "description" => clienttranslate('${actplayer} must play a card'),
+        "descriptionmyturn" => clienttranslate('${you} must play a card'),
+        "type" => "activeplayer",
+        "possibleactions" => array( "playCard" ),
+        "transitions" => array( "playCard" => 32 )
+    ), 
+    32 => array(
+        "name" => "nextPlayer",
+        "description" => "",
+        "type" => "game",
+        "action" => "stNextPlayer",
+        "transitions" => array( "nextPlayer" => 31, "nextTrick" => 30, "endHand" => 40 )
+    ), 
+    
+    
+    40 => array(
+        "name" => "endHand",
+        "description" => "",
+        "type" => "game",
+        "action" => "stEndHand",
+        "transitions" => array( "nextHand" => 20, "endGame" => 99 )
+    ),  
    
     // Final state.
     // Please do not modify (and do not overload action/args methods).
