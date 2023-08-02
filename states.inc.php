233bff174ba5d91519c076c892c953db973c6cb8
swrush98 <swrush98@yahoo.com>
2023-08-01 21:44:38 -0500

    Remove example first state from logic (via commenting)

--- states.inc.php
+++ states.inc.php
@@ -58,19 +58,19 @@ $machinestates = array(
         "description" => "",
         "type" => "manager",
         "action" => "stGameSetup",
-        "transitions" => array( "" => 2 )
+        "transitions" => array( "" => 20 )
     ),
     
     // Note: ID=2 => your first state
 
-    2 => array(
+    /*2 => array(
     		"name" => "playerTurn",
     		"description" => clienttranslate('${actplayer} must play a card or pass'),
     		"descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
     		"type" => "activeplayer",
     		"possibleactions" => array( "playCard", "pass" ),
     		"transitions" => array( "playCard" => 2, "pass" => 2 )
-    ),
+    ),*/
     
 /*
     Examples:
